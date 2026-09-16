461ab0925fe9bcc9a214208f8733e53b92659a05
nikzvar <nzvarykin@mail.ru>
2026-09-16 19:38:37 +0300

    Add double and int scanner methods for check users mistakes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -16,6 +16,10 @@ public class Main {
 
         while (true) {
             System.out.println("Choose a way to move: 1 - walk, 2 - horse ride, 3 - fly, 0 - exit");
+            while (!scanner.hasNextInt()) {
+                System.out.println("Please enter a number");
+                scanner.next();
+            }
             int choice = scanner.nextInt();
 
             if (choice == 0) {
@@ -32,8 +36,17 @@ public class Main {
             }
 
             System.out.print("Enter x: ");
+            while (!scanner.hasNextDouble()) {
+                System.out.print("Please enter a number. Enter x: ");
+                scanner.next();
+            }
             double x = scanner.nextDouble();
+
             System.out.print("Enter y: ");
+            while (!scanner.hasNextDouble()) {
+                System.out.print("Please enter a number. Enter y: ");
+                scanner.next();
+            }
             double y = scanner.nextDouble();
 
             hero.move(new Point(x, y));
